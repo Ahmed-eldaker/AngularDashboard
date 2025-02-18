686aea5c2cb2fd7a211b6481eeef272eafa12110
Ahmed-eldaker <ahmeddaker01@gmail.com>
2025-02-18 11:58:37 +0200

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SidebarComponent } from "./features/components/sidebar/sidebar.component";
+import { NavbarComponent } from "./features/components/navbar/navbar.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [SidebarComponent, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
