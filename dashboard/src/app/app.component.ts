329ed9c6d64beb56abd9c1f4d56a1dc1d69e5533
Ahmed-eldaker <ahmeddaker01@gmail.com>
2025-02-19 15:10:09 +0200

    new

--- dashboard/src/app/app.component.ts
+++ dashboard/src/app/app.component.ts
@@ -1,13 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, viewChild } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SideBarComponent } from "./features/components/side-bar/side-bar.component";
+import { NavBarComponent } from "./features/components/nav-bar/nav-bar.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [ SideBarComponent, NavBarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'dashboard';
+
 }
